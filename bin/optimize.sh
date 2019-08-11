009e8f7a2778f5228a13115ec7bc87933eba259c
ytake <yuuki.takezawa@comnect.jp.net>
2019-08-11 23:08:26 +0900

    added

--- bin/optimize.sh
+++ bin/optimize.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "** Building For Repo Authoritative"
+set -x
+hhvm --hphp --target hhbc \
+  -l3 \
+  --module src \
+  --module boot \
+  --module config \
+  --module vendor \
+  --ffile public/index.hack \
+  --ffile .env \
+  --output-dir /var/out 
+set +x
